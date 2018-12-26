c8deb9bd046ab734798eb09c34d5835621dea29f
aswink1266 <aswin@labglo.com>
2018-12-26 14:21:56 +0530

    edited by aswin 3rd time

--- gitproject/first_pgm.py
+++ gitproject/first_pgm.py
@@ -3,7 +3,7 @@ def main():
     print("this is my waste  pgm")
     print("this is my first pgm")
     print("edited by aswin 2 nd time")
-
+    print("edited by aswin 3rd time ")
 
 if __name__ == '__main__':
     main()
